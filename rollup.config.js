6c26e730bf94b9880c72f736a67fafcf36e05e75
Jop de Klein <jop@validweb.nl>
2020-11-23 18:42:23 +0100

    Add XO support

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,23 @@
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
+import {nodeResolve} from '@rollup/plugin-node-resolve'
+import {terser} from 'rollup-plugin-terser'
 
 // TODO: Package individual packages
 export default {
   input: 'packages/core/src/index.ts',
   output: {
     dir: 'dist',
-    format: 'cjs',
+    format: 'cjs'
   },
-    plugins: [/*terser()*/, nodeResolve({
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-        preferBuiltins: false,
-        browser: true
-  }), commonjs({extensions: ['.js', '.jsx', '.ts', '.tsx']}), typescript()],
+  plugins: [
+    terser(),
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      preferBuiltins: false,
+      browser: true
+    }),
+    commonjs({extensions: ['.js', '.jsx', '.ts', '.tsx']}),
+    typescript()
+  ]
 }
