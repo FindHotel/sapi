e8dccf48259d16e6263b40ce0f5b33ce4cac5a2d
Jop de Klein <jop@validweb.nl>
2021-01-18 20:19:30 +0100

    Add tests and documentation for utils, add type-fest helpers

--- packages/core/src/utils.test.ts
+++ packages/core/src/utils.test.ts
@@ -9,6 +9,14 @@ import {
 const DATE_FORMAT = 'yyyy-MM-dd'
 
 describe('Utils', () => {
+  describe('dateFormat', () => {
+    it('Formats dates', () => {
+      expect(dateFormat(new Date('2018-02-01 13:23:22'), DATE_FORMAT)).toEqual(
+        '2018-02-01'
+      )
+    })
+  })
+
   describe('dateStringToMiddayUTC', () => {
     it('Converts date to midday UTC', () => {
       const dateString = '2018-02-01'
@@ -20,5 +28,74 @@ describe('Utils', () => {
         '2018-02-01'
       )
     })
+  })
+
+  describe('getCheckInNights', () => {
+    it.each([
+      [undefined, undefined],
+      ['2023-02-04', undefined],
+      [undefined, '2023-02-04'],
+      ['', '2023-02-04']
+    ])(
+      'returns undefined with incomplete arguments %s %s',
+      (checkIn, checkOut) => {
+        expect(getCheckInNights(checkIn, checkOut)).toEqual(undefined)
+      }
+    )
+
+    it.each`
+      title                        | checkIn         | checkOut        | expected
+      ${'same day'}                | ${'2023-02-04'} | ${'2023-02-04'} | ${'2023-02-04-00'}
+      ${'one day length of stay'}  | ${'2023-02-04'} | ${'2023-02-05'} | ${'2023-02-04-01'}
+      ${'11 day length of stay'}   | ${'2023-02-04'} | ${'2023-02-15'} | ${'2023-02-04-11'}
+      ${'negative length of stay'} | ${'2023-02-04'} | ${'2023-02-04'} | ${'2023-02-04--01'}
+    `('returns $expected for $title', ({checkIn, checkOut, expected}) => {
+      expect(getCheckInNights('2023-02-04')).toEqual(undefined)
+    })
+  })
+
+  describe('generateSearchId', () => {
+    it('generates a search id', () => {
+      const searchParameters = {
+        checkIn: '2019-05-24',
+        checkOut: '2019-05-26',
+        guestRatings: '7',
+        hotelId: '1212868',
+        rooms: '1|1',
+        sortField: 'popularity',
+        sortOrder: 'ascending',
+        starRatings: ['4', '3']
+      }
+      const sapiOptions = {
+        anonymousId: '84c376fc-9dd7-54c0-876f-4dab09736545',
+        language: 'en',
+        currency: 'EUR',
+        country: 'US'
+      }
+
+      expect(generateSearchId(searchParameters, sapiOptions)).toEqual(
+        'c6303dbf88def3ef723b8fdc67f938614f28ce81'
+      )
+    })
+  })
+
+  describe('omit', () => {
+    it.each`
+      title                             | keys                                  | obj                               | expected
+      ${'existing properties'}          | ${['foo', 'bar']}                     | ${{foo: 'a', baz: 'b', bar: 'c'}} | ${{baz: 'b'}}
+      ${'some non existing properties'} | ${['doesnexist', 'bar']}              | ${{foo: 'a', baz: 'b', bar: 'c'}} | ${{foo: 'a', baz: 'b'}}
+      ${'no matching properties'}       | ${['doesnexist', 'alsodoesnotexist']} | ${{foo: 'a', baz: 'b', bar: 'c'}} | ${{foo: 'a', baz: 'b', bar: 'c'}}
+      ${'no properties'}                | ${[]}                                 | ${{foo: 'a', baz: 'b', bar: 'c'}} | ${{foo: 'a', baz: 'b', bar: 'c'}}
+      ${'empty target object'}          | ${['doesnexist', 'alsodoesnotexist']} | ${{}}                             | ${{}}
+    `(
+      'returns object without specified properties without modifying original object when passing $title',
+      ({title, keys, obj, expected}) => {
+        expect(omit(keys, obj)).toEqual(expected)
+        expect(obj).toEqual(obj)
+
+        const input = {foo: 'a', bar: 3}
+        const output = omit(['foo'], input).bar
+      }
+    )
   })
 })
--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -3,6 +3,8 @@ import differenceInDays from 'date-fns/differenceInDays'
 import parseISO from 'date-fns/parseISO'
 import format from 'date-fns/format'
 
+import {Except} from 'type-fest'
+
 import {AnonymousId, ApiSearchParameters} from './types'
 
 interface GenerateSearchIdOptions {
@@ -12,6 +14,12 @@ interface GenerateSearchIdOptions {
   country: string
 }
 
+/**
+ * Uses date-fns/format to format a date, falling back to undefined in case no date is specified.
+ *
+ * @param date date to format
+ * @param dateFormat format string, see https://date-fns.org/v1.30.1/docs/format
+ */
 export const dateFormat = (
   date: (string | null | undefined) | (Date | null | undefined),
   dateFormat: string
@@ -20,6 +28,13 @@ export const dateFormat = (
 export const dateStringToMiddayUTC = (date: string): Date =>
   parseISO(`${date} 12:00:00`)
 
+/**
+ * Returns a string to use in Algolia tags, in the format: 2021-01-18-02.
+ * The last section is the length of stay in days.
+ *
+ * @param checkIn date string 2021-01-18
+ * @param checkOut date string 2021-01-19
+ */
 export const getCheckInNights = (checkIn?: string, checkOut?: string) => {
   if (!checkIn || !checkOut) return
 
@@ -31,6 +46,12 @@ export const getCheckInNights = (checkIn?: string, checkOut?: string) => {
   return `${checkInFormatted}-${nights}`
 }
 
+/**
+ * Generates a deterministic hash of a combination of ApiSearchParameters and SAPI options.
+ *
+ * @param parameters all relevant search parameters to generate an id
+ * @param options SAPI options used to generate an id
+ */
 export const generateSearchId = (
   parameters: ApiSearchParameters,
   options: GenerateSearchIdOptions
@@ -40,7 +61,18 @@ export const generateSearchId = (
   return hash({...parameters, anonymousId, language, currency, country})
 }
 
-export const omit = (keys: string[], object: any) => {
+/**
+ * Removes specified @keys from @object
+ *
+ * @typeParam K string
+ * @typeParam T object
+ * @param keys keys of the properties to remove from @object
+ * @param object target object to emit @properties from
+ */
+export const omit = <K extends keyof T, T>(
+  keys: K[],
+  object: T
+): Except<T, K> => {
   // eslint-disable-next-line unicorn/no-reduce
   return keys.reduce((acc, key) => {
     const {[key]: omit, ...rest} = acc
